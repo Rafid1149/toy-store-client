94c023b34c2a8da514e412d5f5032bf5840d72b5
rafid149 <rafidtahmid75@gmail.com>
2023-05-21 10:57:23 +0600

    footer added

---
+++ src/Pages/Shared/Footer.jsx
@@ -0,0 +1,36 @@
+
+import logo from '../../assets/logo/Village-Toy-Shop_logo.png'
+
+const Footer = () => {
+    return (
+        <footer className="footer p-10 bg-base-200 text-base-content rounded-2xl">
+        <div>
+          <p className='font-semibold'>© Toy world</p>
+          <img className='w-24 h-24 image-full' src={logo} alt="" />
+          <p className='font-bold text-xl text-purple-600'>Toy World</p>
+        </div> 
+        <div>
+          <span className="footer-title">Services</span> 
+          <a className="link link-hover">Branding</a> 
+          <a className="link link-hover">Design</a> 
+          <a className="link link-hover">Marketing</a> 
+          <a className="link link-hover">Advertisement</a>
+        </div> 
+        <div>
+          <span className="footer-title">Company</span> 
+          <a className="link link-hover">About us</a> 
+          <a className="link link-hover">Contact</a> 
+          <a className="link link-hover">Jobs</a> 
+          <a className="link link-hover">Press kit</a>
+        </div> 
+        <div>
+          <span className="footer-title">Legal</span> 
+          <a className="link link-hover">Terms of use</a> 
+          <a className="link link-hover">Privacy policy</a> 
+          <a className="link link-hover">Cookie policy</a>
+        </div>
+      </footer>
+    );
+};
+
+export default Footer;
---
+++ src/Pages/Shared/Navbar.jsx
@@ -0,0 +1,11 @@
+
+
+const Navbar = () => {
+    return (
+        <div>
+            nav items
+        </div>
+    );
+};
+
+export default Navbar;
