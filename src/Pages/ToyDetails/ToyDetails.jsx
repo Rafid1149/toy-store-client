0723ce3bf7dbb0528d4d5112a53bc8636f23810b
rafid149 <rafidtahmid75@gmail.com>
2023-05-21 22:47:26 +0600

    toy details section added

--- src/Pages/ToyDetails/ToyDetails.jsx
+++ src/Pages/ToyDetails/ToyDetails.jsx
@@ -5,51 +5,59 @@ import useTitle from "../../Hook/useTitle";
 
 
 const SingleToy = () => {
-  const toy = useLoaderData();
-  
-  useTitle(toy?.name);
-  const {
-    name,
-    picture_url,
-    rating,
-    category,
-    price,
-    quantity,
-    description,
-    postedBy,
-  } = toy;
-  return (
-    <div className="w-9/12 mx-auto my-10 bg-base-200">
-      <div className="hero">
-        <div className="hero-content flex-col justify-center gap-5 lg:flex-row">
-          <img src={picture_url} className="w-96 rounded-lg shadow-2xl" />
-          <div>
-            <h2 className="text-5xl font-bold text-primary">{name}</h2>
-            <div className="flex justify-center items-center mt-5">
-              <p>Rating: </p>
-              <Rating
-                readOnly
-                style={{ maxWidth: 150 }}
-                value={rating}
-              ></Rating>
+    const toy = useLoaderData();
+
+    useTitle(toy?.name);
+    const {
+        name,
+        picture_url,
+        rating,
+        category,
+        price,
+        quantity,
+        description,
+        postedBy,
+    } = toy;
+    return (
+
+        <div className="bg-base-200 rounded-xl my-12">
+            <h1 className="text-5xl text-center pt-16 font-bold">{name}</h1>
+            <div className="hero min-h-screen">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <img src={picture_url} className="max-w-sm rounded-lg  shadow-2xl" />
+                    <div>
+                        <div className="">
+                            
+                            <p>Rating: </p>
+                            <Rating
+
+                                style={{ maxWidth: 130 }}
+                                value={rating}
+                            ></Rating>
+                        </div>
+                        {postedBy && <p>Seller: {postedBy}</p>}
+                        <p>Category: {category}</p>
+
+                        <h1 className="text-4xl font-semibold textarea-accent mb-2">
+                            Price: ${price}
+                        </h1>
+
+                        <p>Quantity: {quantity}</p>
+                    </div>
+
+                </div>
+
+
             </div>
-            {postedBy && <p>Seller: {postedBy}</p>}
-            <p>Category: {category}</p>
-            <hr className="w-80 h-1 mx-auto my-4 bg-primary border-0 rounded" />
-            <h1 className="text-4xl font-semibold textarea-accent">
-              Price: ${price}
-            </h1>
-            <hr className="w-80 h-1 mx-auto my-4 bg-primary border-0 rounded" />
-            <p>Quantity: {quantity}</p>
-          </div>
+            <div>
+           <h3 className="text-3xl font-semibold text-center">Description</h3>
+        <p className="w-1/2 mx-auto text-base pb-1">{description}</p>
+         </div>
+            
         </div>
-      </div>
-      <div>
-        <h3 className="text-2xl font-semibold text-center">Description</h3>
-        <p className="w-1/2 mx-auto text-base pb-5">{description}</p>
-      </div>
-    </div>
-  );
+
+       
+    );
 };
 
 export default SingleToy;
